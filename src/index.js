44b09c3327e9e662ff3f2ec1417f79ce5bb0aaca
Stephie27 <stephanieyosh@gmail.com>
2025-07-20 13:21:38 -0400

    added AP API integration

--- src/index.js
+++ src/index.js
@@ -1,12 +1,24 @@
-function generatePoem(event) {
-  event.preventDefault();
-
+function displayPoem(response) {
   new Typewriter("#poem", {
-    strings: "Generating poem",
+    strings: response.data.answer,
     autoStart: true,
     delay: 100,
     cursor: "",
   });
+}
+
+function generatePoem(event) {
+  event.preventDefault();
+
+  let instructionsInput = document.querySelector("#user-instruction");
+
+  let apiKey = "dbc80t431e2275e57a3b2912ao37a0f9";
+  let prompt = `Generate a poem on about ${instructionsInput.value}`;
+  let context =
+    "You are a Haiku expert and love to write haikus. Your mission is to generate a haiku in basic HTML, do Not show the HTML ticks. Separate each line with a <br/>. Display only the Haiku, sign the Haiku with 'SheCodes AI' inside a <strong> element at the end of the Haiku. Make sure to follow the user instructions. ";
+  let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+
+  axios.get(apiUrl).then(displayPoem);
 }
 
 let poemFormElement = document.querySelector("#poem-generator-form");
